dc3e05cb20f1fe7b389638ca9abcc55dbd2230e8
Luiz Paulo <lpmragi@gmail.com>
2026-09-21 18:03:14 -0300

    Hardcode Supabase public keys

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  console.warn('Faltam as variáveis de ambiente NEXT_PUBLIC_SUPABASE_URL ou NEXT_PUBLIC_SUPABASE_ANON_KEY');
-}
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://tnvlllanpuxvfudtqvwg.supabase.co';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRudmxsbGFucHV4dmZ1ZHRxdndnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAwMTkzMzQsImV4cCI6MjEwNTU5NTMzNH0.xHA0-6zOZCeog-NCu-iqdVHLmVWOAoQVfhGRfhnlnRQ';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
